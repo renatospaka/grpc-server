21f8ccd29259d46b8d856e9a6aaff90591cc3807
Renato Spakauskas <renatospaka@gmail.com>
2022-04-02 17:22:39 -0300

    feat: :sparkles: grpc server
    gRPC server attending calls from a HTTP server

--- cmd/server.go
+++ cmd/server.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"net/http"
-	"server/model"
+	"net"
 	"encoding/json"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"server/model"
+	"server/grpc/service"
+	"server/grpc/pb"
 )
 
 var courseList = model.NewCourses()
 
 func main() {
+	// Start gRPC server
+	go startGrpc()
+
+	// Http server
 	http.HandleFunc("/course", CourseListHandler)
 	http.ListenAndServe(":8000", nil)
+}
+
+func startGrpc() {
+	listen, err := net.Listen("tcp", "localhost:50056")
+	if err != nil {
+		panic(err)
+	}
+
+	// gRPC server
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	courseService := service.NewCourseGrpcService()
+	courseService.Courses = courseList
+
+	// Register the gRPC server and serve
+	pb.RegisterCourseServiceServer(grpcServer, courseService)
+	grpcServer.Serve(listen)
 }
 
 func CourseListHandler(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -2,9 +2,18 @@ module server
 
 go 1.18
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/satori/go.uuid v1.2.0
+	google.golang.org/grpc v1.45.0
+	google.golang.org/protobuf v1.28.0
+)
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
